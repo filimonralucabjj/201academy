ae2e38b8d69bcfeb7be61fe8f340ac0043e3293e
Filimon Raluca <raluca.filimon@info.uaic.ro>
2025-02-21 14:20:56 +0200

    added meta

--- src/components/Pricing/Pricing.tsx
+++ src/components/Pricing/Pricing.tsx
@@ -14,7 +14,7 @@ const Pricing = () => {
         <Title title={t("pricingSection.title")} subtitle={t("pricingSection.subtitle")} />
         <div className='flex items-center justify-center mb-5' >
           {pricingItems.map((item: PricingItemType) => (
-            <button key={item.id} type="button" className={` ${active == item.id ? "text-white bg-neutral-900" : "text-neutral-700"} bg-black hover:text-white border border-neutral-800 hover:bg-neutral-900 font-small md:font-medium rounded-lg text-sm md:px-5 py-1 md:py-2.5 text-center me-2 mb-2 h-12 p-2`}
+            <button key={item.id} type="button" className={` ${active == item.id ? "text-white bg-neutral-900" : "text-neutral-200"} bg-black hover:text-white border border-neutral-800 hover:bg-neutral-900 font-small md:font-medium rounded-lg text-sm md:px-5 py-1 md:py-2.5 text-center me-2 mb-2 h-12 p-2`}
               onClick={() => { setActive(item.id) }}>
               {item.title}
             </button>
